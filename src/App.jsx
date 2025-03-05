14bf198bd4a97a9ccd0dc25afbf9077a64a8b23b
Jerremy Asuncion <asuncionjerremyph@gmail.com>
2025-03-05 22:56:56 +0800

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import ToDoList from "./ToDoList";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return <ToDoList />;
+}
+
+export default App;
