3a4bbd78a6d6bb8054ac8ac96538ba0c56b9e893
Jerremy Asuncion <asuncionjerremyph@gmail.com>
2025-03-06 22:26:35 +0800

    Added Styles

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -18,43 +18,47 @@ const ToDoList = () => {
     function moveTaskDown() {}
 
     return (
-        <div className="to-do-list">
-            <h1>To-Do-List</h1>
-            <div>
-                <input
-                    type="text"
-                    placeholder="Enter a task..."
-                    value={newTask}
-                    onChange={handleInputChange}
-                />
-                <button className="add-button" onClick={addTask}></button>
+        <main>
+            <div className="to-do-list">
+                <h1>To-Do-List</h1>
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Enter a task..."
+                        value={newTask}
+                        onChange={handleInputChange}
+                    />
+                    <button className="add-button" onClick={addTask}>
+                        Add
+                    </button>
+                </div>
+                <ol>
+                    {tasks.map((task, index) => (
+                        <li key={index}>
+                            <span className="text">{task}</span>
+                            <button
+                                className="delete-button"
+                                onClick={() => deleteTask(index)}
+                            >
+                                Delete
+                            </button>
+                            <button
+                                className="move-button"
+                                onClick={() => moveTaskUp(index)}
+                            >
+                                👆
+                            </button>
+                            <button
+                                className="move-button"
+                                onClick={() => moveTaskDown(index)}
+                            >
+                                👇
+                            </button>
+                        </li>
+                    ))}
+                </ol>
             </div>
-            <ol>
-                {tasks.map((task, index) => (
-                    <li key={index}>
-                        <span className="text">{task}</span>
-                        <button
-                            className="delete-button"
-                            onClick={() => deleteTask(index)}
-                        >
-                            Delete
-                        </button>
-                        <button
-                            className="move-button"
-                            onClick={() => moveTaskUp(index)}
-                        >
-                            Up
-                        </button>
-                        <button
-                            className="move-button"
-                            onClick={() => moveTaskDown(index)}
-                        >
-                            Down
-                        </button>
-                    </li>
-                ))}
-            </ol>
-        </div>
+        </main>
     );
 };
 
